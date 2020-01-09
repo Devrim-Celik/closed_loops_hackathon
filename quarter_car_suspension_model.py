c0b979f5ea8b364e3456442e0870605e52897a01
Devrim Celik <DevCel@protonmail.com>
2020-01-09 11:58:55 +0100

    proper animation, cleaned code

--- quarter_car_suspension_model.py
+++ quarter_car_suspension_model.py
@@ -16,21 +16,9 @@ def surf(t):
     else:
         return 3 #4
 
-def dot_surf(t): #Approximate an impulse
-    v = 80
-    if v*t > 445  and v*t < 605:
-        return 1.8# 4
-    elif v*t > 745  and v*t < 1005:
-        return 1.5# 4
-    elif v*t > 1195  and v*t < 1455:
-        return 4
-    else:
-        return 0
-
-
 class QuarterSuspensionModel():
 
-    def __init__(self, g = 9.81,  m1 = 400, m2 = 30, b1 = 2000, b2 = 10000, 
+    def __init__(self, g = 9.81,  m1 = 1500, m2 = 50, b1 = 3000, b2 = 10000, 
             k1 = 50000, k2 = 80000):
         """
         Args:
@@ -51,23 +39,53 @@ class QuarterSuspensionModel():
         self.b1 = b1
         self.b2 = b2
 
+        self.image_folder = './img'
+        self.file_name = 'quarter_car_suspension_simulation'
+
+
     def dynamic_model(self, states, t, m1, m2, k1, k2, b1, b2):
-        x1 = states[0] # vertical position of sprung mass
-        v1 = states[1] # vertical velocity of sprung mass
-        x2 = states[2] # vertical position of unsprung mass
-        v2 = states[3] # vertical velocity of unsprung mass
+        """
+        Function with ODEs, used by odeint.
+
+        Args
+            states      4-tuple of all variables
+                            x[0] vertical pos. of sprung mass
+                            x[1] vertical vel. of sprung mass
+                            x[2] vertical pos. of unsprung mass
+                            x[3] vertical vel. of unsprung mass
+            t           time
+            m1          mass of sprung
+            m2          mass of unsprung
+            k1          spring constant 1
+            k2          spring constant 2
+            b1          damping constant 1
+            b2          damping constant 2
+        """
+        x1 = states[0] 
+        v1 = states[1] 
+        x2 = states[2] 
+        v2 = states[3] 
         
+        # change of vertical position of sprung is its vertical velocity
         dx1dt = v1
+        # force of spring and damper
         dv1dt = (- k1 * (x1 - x2) - b1 * (v1 - v2)) / m1  
+        # change of vertical position of unsprung is its vertical velocity
         dx2dt = v2
-        dv2dt = (k1 * (x1 - x2) + b1 * (v1 - v2) - k2 * (x2 - surf(t)) - b2 * (v2 - dot_surf(t)))/m1
+        # force of spring and damper between sprung and unsprung and force of spring and damper of unsprung to ground
+        dv2dt = (k1 * (x1 - x2) + b1 * (v1 - v2) - k2 * (x2 - surf(t)) - b2 * (v2 - (surf(t)-surf(t-1))))/m1
 
         return [dx1dt, dv1dt, dx2dt, dv2dt]
 
 
-    def run_simulation(self, nr_seconds = 20, file_name="quarter_car_suspension_simulation", tire_width = 2.0, tire_height = 1.0, car_width = 3.0, car_height = 1.5):
+    def run_simulation(self, nr_seconds = 20):
         """
-        t           time of simulation in seconds
+        Args
+            t           time of simulation in seconds
+            nr_seconds  amount of time to run simulation in seconds
+
+        Returns
+            sol         solution of ODE, row is time and col is states
         """
         # states with the following states:
         #   states[0] displacement of sprung mass
@@ -77,47 +95,86 @@ class QuarterSuspensionModel():
         states = [0, 0, 0, 0]
 
         # generate time steps in milliseconds
-        t = np.linspace(0, nr_seconds, 250)
-
-        # TODO 
-        surface = [surf(ts) for ts in t] 
+        self.t = np.linspace(0, nr_seconds, 250)
 
         # solve differential equaiton / run simulation
-        sol = odeint(self.dynamic_model, states, t, args=(self.m1, self.m2, self.k1, self.k2, self.b1, self.b2))
+        self.sol = odeint(self.dynamic_model, states, self.t, args=(self.m1, self.m2, self.k1, self.k2, self.b1, self.b2))
+
+        return self.sol
 
 
-        # static plot
+    def static_plot(self):
+        """
+        Constructs a static plot of the displacement of sprung
+        and unsprung mass combined with dispalying the profile of the road.
+        """
+        # TODO 
+        surface = [surf(ts) for ts in self.t] 
+        
         plt.figure(figsize=(16,10))
-        plt.plot(t, sol[:,0], label = "car displacement")
-        plt.plot(t, sol[:,2], label = "tire displacement")
-        plt.plot(t, surface, label = "road")
+        plt.plot(self.t, self.sol[:,0], label = "sprung displacement")
+        plt.plot(self.t, self.sol[:,2], label = "unsprung displacement")
+        plt.plot(self.t, surface, label = "ground")
         plt.legend()
 
+        plt.savefig(self.image_folder + '/' + self.file_name + '.png')
 
-        # animation
+        plt.show()
+
+    def animation(self, unsprung_width = 2.0, unsprung_height = 1.0, sprung_width = 3.0, sprung_height = 1.5):
+        """
+        Creates animation of simulation.
+
+        Args
+            unsprung_width      width of unsprung in animation
+            unsprung_height     height of unsprung in animation
+            sprung_width        width of prung in animation
+            sprung_height       height of sprung in animation
+        """
+
+        # TODO 
+        surface = [surf(ts) for ts in self.t] 
+        
         fig = plt.figure(figsize=(16, 10), facecolor='w')
         ax = fig.add_subplot(1, 1, 1)
         plt.rcParams['font.size'] = 15
-        ax.plot(t, surface, color='k', lw=2)
+        ax.plot(self.t, surface, color='k', lw=2)
 
-        lns = []
-        for i, ts in enumerate(t):
+        draw_objects = []
+        for i, ts in enumerate(self.t):
+            #TODO add springs and dampeners
+            # text of time
             tm = ax.text(ts, 0, 'time = {:06.3f}ms'.format(ts))
+            # blak point touching the grond
             point = ax.add_artist(plt.Circle((ts, surface[i]), 0.2, color='black'))
-            tire = ax.add_patch(patches.Rectangle((ts - tire_width/2, sol[i,2] + 2 - tire_height/2), 
-                width = tire_width, height = tire_height, linewidth = 2, edgecolor = 'black', facecolor = 'blue'))
-            car = ax.add_patch(patches.Rectangle((ts - car_width/2, sol[i,0] + 2 + tire_height + 2 - car_height/2), 
-                width = car_width, height = car_height, linewidth = 2, edgecolor = 'black', facecolor = 'blue'))
-            lns.append([tm, point, tire, car])
+            # unsprung
+            unsprung = ax.add_patch(patches.Rectangle((ts - unsprung_width/2, self.sol[i,2] + 2 - unsprung_height/2), 
+                width = unsprung_width, height = unsprung_height, linewidth = 2, edgecolor = 'black', facecolor = 'blue'))
+            unsprung_txt = ax.text(ts, self.sol[i,2] + 2, "$m2$")
+            # sprung
+            sprung = ax.add_patch(patches.Rectangle((ts - sprung_width/2, self.sol[i,0] + 2 + unsprung_height + 2 - sprung_height/2), 
+                width = sprung_width, height = sprung_height, linewidth = 2, edgecolor = 'black', facecolor = 'blue'))
+            sprung_txt = ax.text(ts, self.sol[i,0] + 2 + unsprung_height + 2,"$m1$")
+
+            draw_objects.append([tm, point, unsprung, sprung, unsprung_txt, sprung_txt])
+
         ax.set_ylim([0, 10])
         ax.set_xlim([-1, 1+ts])
         ax.set_aspect('equal', 'datalim')
         ax.grid()
-        ani = animation.ArtistAnimation(fig, lns, interval=1)
-        #ani.save(file_name + '.gif', writer='imagemagick', fps=1000/50)
+
+        ani = animation.ArtistAnimation(fig, draw_objects, interval=1)
+
+        #saving animation
+        Writer = animation.writers['ffmpeg']
+        writer = Writer(fps=1, metadata=dict(artist='Devrim Celik'), bitrate=1800)
+
+        ani.save(self.image_folder + '/' + self.file_name + '.mp4', writer='writer')
 
         plt.show()
 if __name__=="__main__":
     qsm = QuarterSuspensionModel()
     qsm.run_simulation()
+    qsm.static_plot()
+    qsm.animation()
 
