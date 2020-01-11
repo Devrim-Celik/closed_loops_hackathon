819701ef509196b252b02146b2bf2e36eee7c665
mvidaldp <mvidal.dp@gmail.com>
2020-01-11 15:09:15 +0100

    Add multi-threading

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,5 +1,9 @@
 from auxiliary import time_and_profile
 import csv
+from multiprocessing.pool import Pool
+import multiprocessing
+
+cores = multiprocessing.cpu_count()
 
 profileLocation = 'datasets/'
 # profile filenames
@@ -13,12 +17,23 @@ datasets = [
     'ts3_2_k_3.0.csv',
     'ts3_3_k_3.0.csv'
 ]
+velocities = [i for i in range(5, 31)]  # loop over all velocities
+
+
+def thread_job(iter):
+    fname = iter[0]
+    vel = iter[1]
+    time, profile = time_and_profile(profileLocation + fname, vel)
+    with open(profileLocation + "preproc2/" + fname[:-4] + "_" + str(vel) + ".csv", mode='w') as file:
+        writer = csv.writer(file, delimiter=',')
+        for p in profile:
+            writer.writerow([p])
+
 
 for dataset in datasets:
-    fname = profileLocation + dataset
-    for vel in range(5, 31):  # loop over all velocities
-        time, profile = time_and_profile(fname, vel)
-        with open(profileLocation + "preproc/" + dataset[:-4] + "_" + str(vel) + ".csv", mode='w') as file:
-            writer = csv.writer(file, delimiter=',')
-            for p in profile:
-                writer.writerow([p])
+    threads = list()
+    iterable = [(dataset, vel) for vel in velocities]
+    with Pool(processes=cores) as pool:
+        pool.map(thread_job, iterable)  # vel is the iterable parameter
+        pool.close()
+        pool.join()
